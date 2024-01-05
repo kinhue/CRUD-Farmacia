016bbb0e91cbee39c2f1b1822799888d1320a480
Arthur <arthurreis22@gmail.com>
2024-01-05 03:06:06 -0300

    Finalização CRUD Produto e seus relacionamentos

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,86 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Por favor insira um tipo de categoria!")
+	@Size(min = 5, max = 50, message = "Mínimo de 5 e máximo de 50 caracteres no tipo!")
+	@Column(length = 50)
+	private String tipo;
+
+	@NotBlank(message = "Por favor insira a sigla do país do produto!")
+	@Size(min = 2, max = 3, message = "A sigla deve conter ou 2 ou 3 letras")
+	@Column(length = 3)
+	private String pais;
+
+	@NotBlank(message = "Coloque a marca!")
+	@Size(min = 2, max = 25, message = "A marca deve ser de 2 a 25 letras!")
+	@Column(length = 25)
+	private String marca;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+}
